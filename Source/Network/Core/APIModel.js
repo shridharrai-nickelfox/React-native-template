f5d58ddcaebcfe4f7e007a82998782eacd5cfb52
hiten-nickelfox <hiten-nickelfox@nickelfox.co>
2023-04-16 10:48:25 +0530

    chore(network)- file structure changed

--- Source/Network/Core/APIModel.js
+++
@@ -1,9 +0,0 @@
-// Common model for structuring API endpoints
-
-export class Endpoint {
-  constructor(endpoint, method, version = "v1") {
-    this.endpoint = endpoint
-    this.method = method
-    this.version = version
-  }
-}
--- Source/Network/Core/HttpMethods.js
+++
@@ -1,9 +0,0 @@
-// Constant to define HTTP methods
-
-export const HTTP_METHODS = {
-  POST: "POST",
-  GET: "GET",
-  PUT: "PUT",
-  DEL: "DELETE",
-  PATCH: "PATCH"
-}
